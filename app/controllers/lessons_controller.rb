d1ee65981623dd4c52871027eaaa9ac0297ba0c3
Trevor Bowyer <bowyer@gmail.com>
2015-11-13 22:36:35 +0000

    Added authorization to view lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,23 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_authorization_for_current_lesson, :only => [:show]
+	
 	def show
 	end
 
 	private
 
+	def require_authorization_for_current_lesson
+		course = current_lesson.section.course
+		if !current_user.enrolled_in?(course) 
+				redirect_to course_path(course), alert: 'You must be enrolled to view lessons'
+		end
+	end
+	
 	helper_method :current_lesson
-
+	
+	
+	
 	def current_lesson
 		@current_lesson ||= Lesson.find(params[:id])
 	end
