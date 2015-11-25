6efe638bcf9388efad8af401706b42a125be3222
Trevor Bowyer <bowyer@gmail.com>
2015-11-25 23:18:22 +0000

    test2

--- config/application.rb
+++ config/application.rb
@@ -21,18 +21,6 @@ module Flixter
     # config.i18n.default_locale = :de
     
 
-    initializer 'setup_asset_pipeline', :group => :all  do |app|
-      # We don't want the default of everything that isn't js or css, because it pulls too many things in
-      app.config.assets.precompile.shift
     
-      # Explicitly register the extensions we are interested in compiling
-      app.config.assets.precompile.push(Proc.new do |path|
-        File.extname(path).in? [
-          '.html', '.erb', '.haml',                 # Templates
-          '.png',  '.gif', '.jpg', '.jpeg',         # Images
-          '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
-        ]
-      end)
-    end
   end
 end
