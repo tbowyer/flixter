c2ed7653d6d78d0e52bdd995b515868f9ced3281
Trevor Bowyer <bowyer@gmail.com>
2015-11-25 23:05:09 +0000

    prevented initializing to database

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Flixter::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -77,4 +77,5 @@ Flixter::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
 end
