373eb4b253e5a916d908395b0e1ade8f5693ec76
Trevor Bowyer <bowyer@gmail.com>
2015-11-25 23:16:39 +0000

    test

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Flixter
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.initialize_on_precompile = false
+    
 
     initializer 'setup_asset_pipeline', :group => :all  do |app|
       # We don't want the default of everything that isn't js or css, because it pulls too many things in
